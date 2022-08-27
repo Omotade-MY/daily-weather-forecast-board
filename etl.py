0f89a9d317f64b11f6b0aa341a94e46fbec21560
Muhammad <muhammadyakub181@gmail.com>
2022-08-27 07:47:55 +0100

    Implement upload to s3 bucket

--- etl.py
+++ etl.py
@@ -1,29 +1,51 @@
 
 import pandas as pd
 from data_api import gen_state
-
-from process import ParseFile, to_csv
-
+from process import ParseFile, create_filestreams, load_file, upload_files
+from data_api import gen_state
 
 def process():
 
     state = pd.read_csv("list_of_capitals.csv").dropna()
     capitals = state['Capital'].values
 
+
     # Extract
-    for data in gen_state(capitals):
-    
-        # Transform
-        parser = ParseFile(json_handler=data)
+    weather_data_gen = iter(gen_state(capitals))
+    create_filestreams.has_been_called = False
+    while True:
+        
+        try:
+            jsondata = next(weather_data_gen)
 
-        city = parser.parse_area()
-        weather, astronomy, hourly  = parser.parse_weather()
+            # Transform
+            parser = ParseFile(json_handler=jsondata)
+            data = {}
 
-        # Load (to csv)
-        to_csv('city.csv',city.dict())
-        to_csv('weather.csv', weather.dict())
-        to_csv('astronomy.csv', astronomy.dict())
-        to_csv('metadata.csv', hourly)
+            city = parser.parse_area()
+            weather, astronomy, hourly  = parser.parse_weather()
+
+            
+
+            data['city'] = city.dict()
+            data['weather'] = weather.dict()
+            data['astronomy'] = astronomy.dict()
+            data['hourly'] = hourly
+
+            # Load to s3
+
+            if create_filestreams.has_been_called == False:
+                create_filestreams(data=data)
+
+            load_file(data=data)
+
+        except StopIteration:
+            print("Loading files completed!\nUploading files to aws s3 bucket...")
+            upload_files()
+            print('Uploaded')
+            break
+
+
 
 
 
